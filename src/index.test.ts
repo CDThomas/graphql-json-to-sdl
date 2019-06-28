a0822e59eb6aa17433464b140a0a166ad13af53b
Drew Thomas <cdthomas.92@gmail.com>
2019-06-28 14:29:53 +1000

    Add basic implemention and test

---
+++ src/index.test.ts
@@ -0,0 +1,91 @@
+import { test } from "@oclif/test";
+import fs from "fs";
+import path from "path";
+
+import cmd = require("../src");
+
+// https://github.com/apollographql/apollo-tooling/blob/e8d432654ea840b9d59bf1f22a9bc37cf50cf800/packages/apollo/src/commands/client/__tests__/generate.test.ts
+
+const deleteFolderRecursive = (path: string) => {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(function(file, index) {
+      var curPath = path + "/" + file;
+      if (fs.lstatSync(curPath).isDirectory()) {
+        // recurse
+        deleteFolderRecursive(curPath);
+      } else {
+        // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+
+    fs.rmdirSync(path);
+  }
+};
+
+const makeNestedDir = (dir: string) => {
+  if (fs.existsSync(dir)) return;
+
+  try {
+    fs.mkdirSync(dir);
+  } catch (err) {
+    if (err.code == "ENOENT") {
+      makeNestedDir(path.dirname(dir)); //create parent dir
+      makeNestedDir(dir); //create dir
+    }
+  }
+};
+
+const setupFS = (files: Record<string, string>) => {
+  let dir: string | undefined;
+  return {
+    async run(ctx: any, ...rest: any) {
+      // make a random temp dir & chdir into it
+      dir = fs.mkdtempSync("__tmp__");
+      process.chdir(dir);
+      // fill the dir with `files`
+      Object.keys(files).forEach(key => {
+        if (key.includes("/")) makeNestedDir(path.dirname(key));
+        fs.writeFileSync(key, files[key]);
+      });
+    },
+    finally(ctx: any) {
+      process.chdir("../");
+      deleteFolderRecursive(dir as string);
+    }
+  };
+};
+
+// helper function to resolve files from the actual filesystem
+const resolveFiles = (opts: { [testPath: string]: string }) => {
+  let files: { [testPath: string]: string } = {};
+  Object.keys(opts).map(key => {
+    files[key] = fs.readFileSync(path.resolve(__dirname, opts[key]), {
+      encoding: "utf-8"
+    });
+  });
+
+  return files;
+};
+
+describe("graphql-json-to-sdl", () => {
+  test
+    .register("fs", setupFS)
+    .fs(
+      resolveFiles({
+        "./schema.json": "../__fixtures__/schema.json"
+      })
+    )
+    .do(() => cmd.run(["./schema.json", "./schema.graphql"]))
+    .it("writes a file", () => {
+      expect(fs.readFileSync("./schema.graphql").toString()).toMatchSnapshot();
+    });
+});
+
+/*
+  TODO:
+  - Test that types/fields in different order in input result in output
+    with sorted types and fields.
+  - Test invalid src (non-existant file, malformed JSON, etc)
+  - Test output errors
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,73 @@
-import {Command, flags} from '@oclif/command'
+import fs from "fs";
+import { Command } from "@oclif/command";
+import { buildClientSchema, printSchema } from "graphql";
+
+interface GraphQLType {
+  readonly name: string | null;
+  readonly fields: GraphQLField[] | null;
+}
+
+interface GraphQLField {
+  readonly name: string;
+}
 
 class GraphqlJsonToSdl extends Command {
-  static description = 'describe the command here'
+  static description = "describe the command here";
 
-  static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
-
-  static args = [{name: 'file'}]
+  static args = [
+    { name: "src", required: true },
+    { name: "out", required: true }
+  ];
 
   async run() {
-    const {args, flags} = this.parse(GraphqlJsonToSdl)
+    const { args } = this.parse(GraphqlJsonToSdl);
+    const { src, out } = args;
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    writeSchema(src, out);
   }
 }
 
-export = GraphqlJsonToSdl
+function writeSchema(src: string, out: string) {
+  const fileContent = fs.readFileSync(src, "utf-8");
+
+  if (!fileContent) {
+    throw new Error("no schema found");
+    return;
+  }
+
+  const { data } = JSON.parse(fileContent);
+
+  data.__schema.types.sort((a: GraphQLType, b: GraphQLType) => {
+    if (!a.name || !b.name) {
+      return compareUnnamedTypes(a, b);
+    }
+
+    return a.name.localeCompare(b.name);
+  });
+
+  data.__schema.types.forEach((type: GraphQLType) => {
+    if (!type.fields) return;
+
+    type.fields.sort((a: GraphQLField, b: GraphQLField) => {
+      return a.name.localeCompare(b.name);
+    });
+  });
+
+  const clientSchema = buildClientSchema(data);
+
+  const graphqlSchemaString = printSchema(clientSchema);
+
+  fs.writeFileSync(out, graphqlSchemaString);
+}
+
+function compareUnnamedTypes(a: GraphQLType, b: GraphQLType): number {
+  if (!isNamedType(a) && isNamedType(b)) return -1;
+  if (isNamedType(a) && !isNamedType(b)) return 1;
+  return 0;
+}
+
+function isNamedType(type: GraphQLType): boolean {
+  return !!type.name;
+}
+
+export = GraphqlJsonToSdl;
