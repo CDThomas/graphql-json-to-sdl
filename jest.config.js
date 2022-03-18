5c9f219e27ea9e4331c1ad5f431cb5a87033c75c
Drew Thomas <cdthomas.92@gmail.com>
2022-03-19 10:19:31 +1100

    Report coverage
    * Bump jest
    
    * Add post coverage step to CI workflow
    
    * Fail tests when coverage is under 100%

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,13 @@ module.exports = {
   roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.tsx?$": "ts-jest"
+  },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
   }
 };
